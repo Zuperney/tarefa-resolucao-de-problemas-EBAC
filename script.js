82cd2707322f2338661ca00c980021a1e7a74b82
Dayvid Ferreira <neymoma@gmail.com>
2025-06-08 19:36:23 -0300

    adicionado if else

--- script.js
+++ script.js
@@ -7,20 +7,46 @@
 //    |4.1 valores de cada categoria
 // 5 - atualizar interface
 // 6 - limpar campos de entrada
-document.getElementById("adicionar").addEventListener("click", function () {
-  const valorInput = document.getElementById("valorReais");
-  console.log(valorInput.value);
-});
 
 const listaGastos = [
   ["Alimentação", 0],
   ["Transporte", 0],
-  ["Saúde", 0],
-  ["Educação", 0],
   ["Lazer", 0],
   ["Outros", 0],
 ];
 
-document.getElementById("atividade").addEventListener("change", function () {
-  console.log(this.value);
-});
+document
+  .getElementById("adicionarGasto", "atividade")
+  .addEventListener("click", function () {
+    const valorInput = document.getElementById("valorReais").value;
+    const opcaoSelect = document.getElementById("atividade").value;
+
+    if (opcaoSelect === "alimentacao") {
+      listaGastos[0][1] += parseFloat(valorInput);
+      console.log("A categoria é Alimentação", listaGastos[0]);
+    } else if (opcaoSelect === "transporte") {
+      listaGastos[1][1] += parseFloat(valorInput);
+      console.log("A categoria é Transporte", listaGastos[1]);
+    } else if (opcaoSelect === "lazer") {
+      listaGastos[2][1] += parseFloat(valorInput);
+      console.log("A categoria é Lazer", listaGastos[2]);
+    } else if (opcaoSelect === "outros") {
+      listaGastos[3][1] += parseFloat(valorInput);
+      console.log("A categoria é Outros", listaGastos[3]);
+    }
+    const textoA = document.getElementById("a");
+    textoA.innerText = `Alimentação: R$ ${listaGastos[0][1].toFixed(2)}`;
+    const textoB = document.getElementById("b");
+    textoB.innerText = `Transporte: R$ ${listaGastos[1][1].toFixed(2)}`;
+    const textoC = document.getElementById("c");
+    textoC.innerText = `Lazer: R$ ${listaGastos[2][1].toFixed(2)}`;
+    const textoD = document.getElementById("d");
+    textoD.innerText = `Outros: R$ ${listaGastos[3][1].toFixed(2)}`;
+    const totalGastos =
+      listaGastos[0][1] +
+      listaGastos[1][1] +
+      listaGastos[2][1] +
+      listaGastos[3][1];
+    const totalTexto = document.getElementById("e");
+    totalTexto.innerText = `Total: R$ ${totalGastos.toFixed(2)}`;
+  });
